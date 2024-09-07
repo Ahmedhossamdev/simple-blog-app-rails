3b8783a57ae2a9b53b665ebd7313d0fd4265cd34
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2024-09-08 00:50:01 +0300

    feat: status

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,14 @@ class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_rich_text :body
 
+  VALID_STATUSES = [ "public", "private", "archived" ]
+
+  validates :status, inclusion: { in: VALID_STATUSES }
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
+
+
+  def archived?
+    status == "archived"
+  end
 end
