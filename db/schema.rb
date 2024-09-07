4787dfcc3276b9999d670f72b5f99740726bb29d
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2024-09-08 00:51:17 +0300

    feat: status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_07_154707) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_07_201239) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -55,6 +55,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_07_154707) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "views"
+    t.string "status"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -63,6 +64,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_07_154707) do
     t.integer "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
