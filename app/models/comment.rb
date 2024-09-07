0fc57c08db4310b8174e9ba2c2690f982b7a88ef
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2024-09-08 00:50:05 +0300

    feat: status

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,23 @@
 class Comment < ApplicationRecord
   belongs_to :article
 
+
+  VALID_STATUSES = [ "public", "private", "archived" ]
+
+  validates :status, inclusion: { in: VALID_STATUSES }
+
   validates :commenter, presence: true
   validates :body, presence: true
+
+  before_create :ensure_created_at
+
+  private
+
+  def ensure_created_at
+    self.created_at ||= Time.current
+  end
+
+  def archived?
+    status == "archived"
+  end
 end
